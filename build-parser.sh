57f6d58a2684dcc27eb504dd56fa31d796ebd73c
Taine Zhao <twshere@outlook.com>
2020-01-27 17:08:44 +0900

    fix cross-platform issue

--- build-parser.sh
+++ build-parser.sh
@@ -1,4 +1,4 @@
-rbnf-pygen.exe ./muridesu.exrbnf ./muridesu.rlex ./parser.py --k 1 --traceback
+rbnf-pygen ./muridesu.exrbnf ./muridesu.rlex ./parser.py --k 1 --traceback
 echo "from ast import *"  > ./muridesu/parser.py
 echo "`cat ./parser.py`" >> ./muridesu/parser.py
 rm ./parser.py
